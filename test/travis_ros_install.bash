9ad6a61d0a90cecc78871102a24afef0669ea0e9
Takeshi Kataoka <rider@sguc.ac.jp>
2017-10-02 09:00:03 +0000

    Add new files

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,8 +8,8 @@ pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/zzzzrider/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
+git clone https://github.com/zzzzrider/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
 bash ./step0.bash
 bash ./step1.bash
 
